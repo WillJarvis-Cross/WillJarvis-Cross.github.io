c3ad60468a825814c416046c7a5d5b3fcd636f98
WillJarvis-Cross <72054183+WillJarvis-Cross@users.noreply.github.com>
2023-05-18 20:54:45 -0400

    temporary additions

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Zmage from "react-zmage";
 import Fade from "react-reveal";
 
 let id = 0;
@@ -8,12 +7,13 @@ class Portfolio extends Component {
     if (!this.props.data) return null;
 
     const projects = this.props.data.projects.map(function (projects) {
-      let projectImage = "images/portfolio/" + projects.image;
-
+      
+      let projectImage = "./images/portfolio/" + projects.image;
+      //<Zmage alt={projects.title} src={projectImage} backdrop="url(images/portfolio/infinityDugeons.gif) no-repeat center fixed 0.5" />
       return (
         <div key={id++} className="columns portfolio-item">
           <div className="item-wrap">
-            <Zmage alt={projects.title} src={projectImage} />
+            <img src={projectImage} alt={projects.title} />
             <div style={{ textAlign: "center" }}>{projects.title}</div>
           </div>
         </div>
@@ -25,11 +25,12 @@ class Portfolio extends Component {
         <Fade left duration={1000} distance="40px">
           <div className="row">
             <div className="twelve columns collapsed">
-              <h1>Check Out Some of My Works.</h1>
+              <h1>Check Out Some of My Favourite Projects.</h1>
 
               <div
                 id="portfolio-wrapper"
-                className="bgrid-quarters s-bgrid-thirds cf"
+                className="bgrid-halves s-bgrid-thirds cf"
+                
               >
                 {projects}
               </div>
