1a136992d71acf779c077e21da69547cacb539f6
WillJarvis-Cross <72054183+WillJarvis-Cross@users.noreply.github.com>
2023-05-23 23:16:16 -0400

    fixed sizing issues desktop

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,7 +8,6 @@ class About extends Component {
     const name = this.props.data.name;
     const profilepic = "images/" + this.props.data.image;
     const bio = <p>Hi! I am a fourth year undergraduate student at the University of Toronto, and I am completing a double major in <em>computer science</em> and <em>astronomy and astrophysics</em> as well as a minor in <em>mathematics</em>. Currently I work for Super (previously Snapcommerce), a commerce company dedicated to cater towards those left behind by credit companies. Outside of my studies, I enjoy learning about the intricacies of our Universe and staying physically active by playing various sports with friends.</p>;
-    const street = this.props.data.address.street;
     const city = this.props.data.address.city;
     const province = this.props.data.address.province;
     const phone = this.props.data.phone;
@@ -36,8 +35,6 @@ class About extends Component {
                     <span>{name}</span>
                     <br />
                     <span>
-                      {street}
-                      <br />
                       {city}, {province}
                     </span>
                     <br />
