2e836d8d5e2494e9afc1083722ad07e2ad9580ac
wjarviscross <wjarviscross@gmail.com>
2024-08-23 12:51:23 -0400

    new resume

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,7 +7,7 @@ class About extends Component {
 
     const name = this.props.data.name;
     const profilepic = "images/" + this.props.data.image;
-    const bio = <p>Hi! I recently graduated from the University of Toronto for computer science, and am currently looking for job opportunities. I also just concluded my 2-year job at Super.com (previously Snapcommerce), a commerce company dedicated to cater towards those left behind by credit companies. Outside of my studies, I enjoy learning about the intricacies of our Universe and staying physically active by playing various sports with friends.</p>;
+    const bio = <p>Hi! I recently graduated from the University of Toronto with a degree in computer science, and I am currently looking for job opportunities. I also just concluded my 2-year job at Super.com (previously Snapcommerce), a commerce company dedicated to catering to those left behind by credit companies. Outside of my studies, I enjoy learning about the intricacies of our Universe and staying physically active by playing various sports with friends.</p>;
     const city = this.props.data.address.city;
     const province = this.props.data.address.province;
     const phone = this.props.data.phone;
