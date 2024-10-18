56776ef5800852bf53315f9d8f5c2afe88b451e4
WillJarvis-Cross <wjarviscross@gmail.com>
2024-10-18 14:26:07 -0400

    add email feature

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,14 +1,50 @@
 import React, { Component } from "react";
-import { motion } from 'framer-motion'
+import emailjs from "emailjs-com";
+import { motion } from 'framer-motion';
 
 class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      message: '',
+      successMessage: '',
+      errorMessage: ''
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { name, email, message } = this.state;
+    console.log("Sending Email with the following data:", { name, email, message })
+
+    // Send email using EmailJS
+    emailjs.send('service_veqgezf', 'template_z36zbqj', {
+      name,
+      email,
+      message
+    }, 'c3JY3KXQoyJde0vSQ')
+    .then((response) => {
+      this.setState({ successMessage: "Your message was sent, thank you!", errorMessage: '' });
+    })
+    .catch((err) => {
+      this.setState({ errorMessage: "Failed to send message, please try again.", successMessage: '' });
+    });
+  };
+
   render() {
     if (!this.props.data) return null;
 
+    const { successMessage, errorMessage } = this.state;
     const name = this.props.data.name;
     const city = this.props.data.address.city;
     const phone = this.props.data.phone;
-    const showEmailStuff = false
 
     return (
       <section id="contact">
@@ -19,114 +55,57 @@ class Contact extends Component {
         >
           <div className="row section-head">
             <div className="two columns header-col">
-              <h1>
-                <span>Get In Touch.</span>
-              </h1>
+              <h1><span>Get In Touch.</span></h1>
             </div>
-
             <div className="ten columns">
-              <p className="lead">Please feel free to contact me through <a href="https://www.linkedin.com/in/williamjarviscross-1378361b6/" target="_blank" rel="noreferrer" >LinkedIn</a> or email at <a href="mailto:wjarviscross@gmail.com">wjarviscross@gmail.com</a></p>
+              <p className="lead">
+                Please feel free to contact me through <a href="https://www.linkedin.com/in/williamjarviscross-1378361b6/" target="_blank" rel="noreferrer">LinkedIn</a> or email at <a href="mailto:wjarviscross@gmail.com">wjarviscross@gmail.com</a>
+              </p>
             </div>
           </div>
         </motion.div>
 
-        {showEmailStuff && <div className="row">
-          <motion.div
-            initial={{ x: -100 }}
-            animate={{ x: 0 }}
-            transition={{ duration: 1 }}
-          >
+        <div className="row">
+          <motion.div initial={{ x: -100 }} animate={{ x: 0 }} transition={{ duration: 1 }}>
             <div className="eight columns">
-              <form action="" method="post" id="contactForm" name="contactForm">
+              <form onSubmit={this.handleSubmit}>
                 <fieldset>
                   <div>
-                    <label htmlFor="contactName">
-                      Name <span className="required">*</span>
-                    </label>
-                    <input
-                      type="text"
-                      defaultValue=""
-                      size="35"
-                      id="contactName"
-                      name="contactName"
-                      onChange={this.handleChange}
-                    />
+                    <label htmlFor="contactName">Name <span className="required">*</span></label>
+                    <input type="text" size="35" id="contactName" name="name" onChange={this.handleChange} />
                   </div>
 
                   <div>
-                    <label htmlFor="contactEmail">
-                      Email <span className="required">*</span>
-                    </label>
-                    <input
-                      type="text"
-                      defaultValue=""
-                      size="35"
-                      id="contactEmail"
-                      name="contactEmail"
-                      onChange={this.handleChange}
-                    />
+                    <label htmlFor="contactEmail">Email <span className="required">*</span></label>
+                    <input type="text" size="35" id="contactEmail" name="email" onChange={this.handleChange} />
                   </div>
 
                   <div>
-                    <label htmlFor="contactSubject">Subject</label>
-                    <input
-                      type="text"
-                      defaultValue=""
-                      size="35"
-                      id="contactSubject"
-                      name="contactSubject"
-                      onChange={this.handleChange}
-                    />
+                    <label htmlFor="contactMessage">Message <span className="required">*</span></label>
+                    <textarea cols="50" rows="15" id="contactMessage" name="message" onChange={this.handleChange}></textarea>
                   </div>
 
                   <div>
-                    <label htmlFor="contactMessage">
-                      Message <span className="required">*</span>
-                    </label>
-                    <textarea
-                      cols="50"
-                      rows="15"
-                      id="contactMessage"
-                      name="contactMessage"
-                    ></textarea>
-                  </div>
-
-                  <div>
-                    <button className="submit">Submit</button>
-                    <span id="image-loader">
-                      <img alt="" src="images/loader.gif" />
-                    </span>
+                    <button type="submit" className="submit">Submit</button>
+                    <span id="image-loader"><img alt="" src="images/loader.gif" /></span>
                   </div>
                 </fieldset>
               </form>
 
-              <div id="message-warning"> Error boy</div>
-              <div id="message-success">
-                <i className="fa fa-check"></i>Your message was sent, thank you!
-                <br />
-              </div>
+              {successMessage && <div id="message-success"><i className="fa fa-check"></i>{successMessage}<br /></div>}
+              {errorMessage && <div id="message-warning">{errorMessage}</div>}
             </div>
           </motion.div>
 
-          <motion.div
-            initial={{ x: -100 }}
-            animate={{ x: 0 }}
-            transition={{ duration: 1 }}
-          >
+          <motion.div initial={{ x: -100 }} animate={{ x: 0 }} transition={{ duration: 1 }}>
             <aside className="four columns footer-widgets">
               <div className="widget widget_contact">
                 <h4>Address and Phone</h4>
-                <p className="address">
-                  {name}
-                  <br />
-                  {city}, Canada
-                  <br />
-                  <span>{phone}</span>
-                </p>
+                <p className="address">{name}<br />{city}, Canada<br /><span>{phone}</span></p>
               </div>
             </aside>
           </motion.div>
-        </div>}
+        </div>
       </section>
     );
   }
