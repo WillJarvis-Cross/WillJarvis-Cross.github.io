333059e50232b59999f4601ba695cc2f128d78b6
WillJarvis-Cross <wjarviscross@gmail.com>
2025-04-05 15:18:33 -0400

    update work

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,8 +7,7 @@ class About extends Component {
 
     const name = this.props.data.name;
     const profilepic = "images/" + this.props.data.image;
-    const bio = <p>Hi! I recently graduated from the University of Toronto with a degree in computer science, and I am currently looking for job opportunities. I also just concluded my 2-year job at Super.com (previously Snapcommerce), a commerce company dedicated to catering to those left behind by credit companies. Outside of my studies, I enjoy learning about the intricacies of our Universe and staying physically active by playing various sports with friends.</p>;
-    const city = this.props.data.address.city;
+    const bio = <p>Hi! I recently graduated from the University of Toronto with a degree in computer science, and I am currently working as a Full Stack Web Developer at Optimize Wealth Management. At Optimize, I develop and maintain a financial management platform using TypeScript and React, enabling users to efficiently manage their finances. Previously, I worked for 2 years at Super.com (previously Snapcommerce), a commerce company dedicated to catering to those left behind by credit companies. Outside of my work, I enjoy learning about the intricacies of our Universe and staying physically active by playing various sports with friends.</p>;    const city = this.props.data.address.city;
     const province = this.props.data.address.province;
     const phone = this.props.data.phone;
     const email = this.props.data.email;
